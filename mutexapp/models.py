dd151206ac5ce7652cffb05bbeb5c7128969cb9f
billumandal <billu.mandal@gmail.com>
2016-11-19 13:10:52 +0530

    initial commit

---
+++ mutexapp/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Mutexs(models.Model):
+    mutexs = models.CharField(max_length=500) 
+
+    def __unicode__(self):
+        return self.mutexs
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    company = models.CharField(max_length=100, blank=True)
+    website = models.CharField(max_length=70, blank=True)
+    email_address = models.EmailField(max_length=100, blank=True)
+    message = models.CharField(max_length=500, blank=True)
+    
---
+++ mutexapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, render_to_response
+from models import Mutexs
+
+def search(request):
+    if request.GET:
+        form = MutexsSearchForm(request.GET)
+        if form.is_valid():
+            result = Mutexs.objects.filter(name__icontains=searchterm)
+        else:
+            results=[]
+
+    else:
+        form = MutexsSearchForm()
+        results=[]
+
+    return render_to_response('search.html', RequestContext(request, {
+            'form':form,
+            'results':results,
+        })
+    )
+
+def ajax_search(request):
+    if request.method == 'POST':
+        search_text = request.POST['search_text']
+        if search_text == '':
+            Mutexs.objects.create(
+                mutexs = request.POST['search_text']
+            )
+    else:
+        search_text = ''
+
+    mutexes = Mutexs.objects.filter(mutexs__contains=search_text)
+
+    return render_to_response('search_results.html', {'mutexes' : mutexes})
+
+def mutexs(request):
+    args = {}
+    args.update(csrf(request))
+
+    args['mutexs'] = Mutexs.objects.all()
+
+def show_header(request):
+    return render(request, 'header.html')

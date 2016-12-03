0e5fb728022a3327fed54199515859a200ebc0f7
billumandal <billu.mandal@gmail.com>
2016-12-03 12:21:08 +0530

    changes to database models

--- mutexapp/forms.py
+++ mutexapp/forms.py
@@ -2,4 +2,12 @@ from django import forms
 from models import Mutexs, Feedback	
 
 class MutexSearchForm(forms.Form):
-    mutexs = forms.CharField(label='search', max_length=500) 
+    mutexs = forms.CharField(label='', max_length=500) 
+
+class FeedbackForm(forms.Form):
+    name = forms.CharField(max_length=100, label='')
+    company = forms.CharField(max_length=100, label='')
+    website = forms.CharField(max_length=70,label='')
+    email_address = forms.EmailField(max_length=100, label='')
+    message = forms.CharField(max_length=500, label='')
+    
--- mutexapp/models.py
+++ mutexapp/models.py
@@ -11,6 +11,5 @@ class Feedback(models.Model):
     name = models.CharField(max_length=100, blank=False)
     company = models.CharField(max_length=100, blank=True)
     website = models.CharField(max_length=70, blank=True)
-    email_address = models.EmailField(max_length=100, blank=True)
-    message = models.CharField(max_length=500, blank=True)
-    
+    email_address = models.EmailField(max_length=100, blank=False)
+    message = models.CharField(max_length=500, blank=False)

e2068440dd9b87605cf0fd39005f25dd77828f48
billumandal <billu.mandal@gmail.com>
2016-12-06 00:03:54 +0530

    Done with the middleware

--- mutexapp/admin.py
+++ mutexapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Feedback, Mutexs
+from models import Feedback, Mutexs, Userlog
 
 # Register your models here.
 class MutexsAdmin(admin.ModelAdmin):
@@ -8,5 +8,9 @@ class MutexsAdmin(admin.ModelAdmin):
 class FeedbackAdmin(admin.ModelAdmin):
     pass
 
+class UserlogAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Mutexs, MutexsAdmin)
 admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(Userlog, UserlogAdmin)
